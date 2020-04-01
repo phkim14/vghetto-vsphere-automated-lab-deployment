4e30e5d8c20e6cb5f62e490e4804367b445b4b87
phkim14 <32853750+phkim14@users.noreply.github.com>
2020-04-01 14:01:08 -0700

    Update add-vds.ps1

--- add-vds.ps1
+++ add-vds.ps1
@@ -10,9 +10,15 @@ $NetworkSubnet = "192.168.30."
 
 
 $MgmtVDSName = "management-VDS"
+$EdgeVDSName = "edge-VDS"
 $MgmtDVPGName = "Management Network"
 $vMotionDVPGName = "vMotion Network"
 $VSANDVPGName = "VSAN Network"
+
+$VSSName = "vSwitch0"
+$MTU = "1700"
+$trunkDVPGName = "trunk1"
+$VLAN4095 = "0-4094"
 
 #My-Logger "Connecting to the VCSA ..."
 $vc = Connect-VIServer $VCSAIPAddress -User "administrator@$VCSASSODomainName" -Password $VCSASSOPassword -WarningAction SilentlyContinue
@@ -23,6 +29,8 @@ $vc = Connect-VIServer $VCSAIPAddress -User "administrator@$VCSASSODomainName" -
 #New-VDPortgroup -VDSwitch $MgmtVDSName -Name $vMotionDVPGName -VlanId $ESXiVLAN -Confirm:$false | Out-Null
 #New-VDPortgroup -VDSwitch $MgmtVDSName -Name $VSANDVPGName -VlanId $ESXiVLAN -Confirm:$false | Out-Null
 
+#New-VDSwitch -Name $EdgeVDSName -Location $NewVCDatacenterName -NumUplinkPorts 2 -Mtu $MTU
+New-VDPortgroup -VDSwitch $EdgeVDSName -Name $trunkDVPGName -VlanTrunkRange $VLAN4095 -Confirm:$false | Out-Null
 
 $vmhosts = Get-Cluster -Server $vc -Name $NewVCVSANClusterName | Get-VMHost
 foreach ($vmhost in $vmhosts) {
@@ -30,23 +38,32 @@ foreach ($vmhost in $vmhosts) {
 	#$vmhostNetworkAdapter2 = Get-VMhost $vmhost | Get-VMHostNetworkAdapter -Physical -Name vmnic1
 	#Get-VDSwitch -Name $MgmtVDSName | Add-VDSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $vmhostNetworkAdapter2 -Confirm:$false | Out-Null
 
+
+    #Get-VDSwitch -Name $EdgeVDSName | Add-VDSwitchVMHost -VMHost $vmhost
+    $vmhostNetworkAdapter1 = Get-VMHost $vmhost | Get-VMHostNetworkAdapter -Physical -Name vmnic0
+    #Get-VDSwitch -Name $EdgeVDSName | Add-VDSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $vmhostNetworkAdapter1 -Confirm:$false | Out-Null
+
+
 	#$dvportgroup = Get-VDPortgroup -name $MgmtDVPGName -VDSwitch $MgmtVDSName
 	#$vmk = Get-VMHostNetworkAdapter -Name vmk0 -VMHost $vmhost
 	#Set-VMHostNetworkAdapter -PortGroup $dvportgroup -VirtualNic $vmk -Confirm:$false | Out-Null
 
+    #Remove-VirtualSwitch -VirtualSwitch $VSSName -Confirm:$false 
+
+
 	#$vmhostNetworkAdapter1 = Get-VMHost $vmhost | Get-VMHostNetworkAdapter -Physical -Name vmnic0
 	#Get-VDSwitch -Name $MgmtVDSName | Add-VDSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $vmhostNetworkAdapter1 -Confirm:$false | Out-Null
 
-    $vmk0 = Get-VMHostNetworkAdapter -Server $vc -Name vmk0 -VMHost $vmhost
-    $vMotionlastNetworkOctetInt = [int]$vmk0.ip.Split('.')[-1] + 10
-    $vMotionlastNetworkOctet = [string]$vMotionlastNetworkOctetInt
-    $VSANlastNetworkOctetInt = [int]$vmk0.ip.Split('.')[-1] + 20
-    $VSANlastNetworkOctet = [string]$VSANlastNetworkOctetInt
-    $vMotionVmkIP = $NetworkSubnet + $vMotionlastNetworkOctet
-    $VSANVmkIP = $NetworkSubnet + $VSANlastNetworkOctet
+    #$vmk0 = Get-VMHostNetworkAdapter -Server $vc -Name vmk0 -VMHost $vmhost
+    #$vMotionlastNetworkOctetInt = [int]$vmk0.ip.Split('.')[-1] + 10
+    #$vMotionlastNetworkOctet = [string]$vMotionlastNetworkOctetInt
+    #$VSANlastNetworkOctetInt = [int]$vmk0.ip.Split('.')[-1] + 20
+    #$VSANlastNetworkOctet = [string]$VSANlastNetworkOctetInt
+    #$vMotionVmkIP = $NetworkSubnet + $vMotionlastNetworkOctet
+    #$VSANVmkIP = $NetworkSubnet + $VSANlastNetworkOctet
 
     #New-VMHostNetworkAdapter -VMHost $vmhost -PortGroup $vMotionDVPGName -VirtualSwitch $MgmtVDSName -IP $vMotionVmkIP -SubnetMask $VMNetmask -Mtu 1600 -VMotionEnabled:$true 
-    New-VMHostNetworkAdapter -VMHost $vmhost -PortGroup $VSANDVPGName -VirtualSwitch $MgmtVDSName -IP $VSANVmkIP -SubnetMask $VMNetmask -Mtu 1600 -VMotionEnabled:$false -VsanTrafficEnabled:$true 
+    #New-VMHostNetworkAdapter -VMHost $vmhost -PortGroup $VSANDVPGName -VirtualSwitch $MgmtVDSName -IP $VSANVmkIP -SubnetMask $VMNetmask -Mtu 1600 -VMotionEnabled:$false -VsanTrafficEnabled:$true 
 
 }
 
